97a5b7606f1c564cd54f64b54af95f414064748a
georgiosan <georgiosanagnostou@hotmail.com>
2021-10-26 19:29:10 +0300

    Adding config files

---
+++ webpack.config.js
@@ -0,0 +1,94 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: './src/index.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    hot: true,
+    port: 3000,
+  },
+  watchOptions: {
+    ignored: /node_modules/,
+  },
+  performance: {
+    maxEntrypointSize: 1536000,
+    maxAssetSize: 1536000,
+  },
+  plugins: [
+    new Dotenv(),
+    new webpack.ids.HashedModuleIdsPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    // new CopyWebpackPlugin({
+    //   patterns: [{ from: './src/icons/', to: './icons' }],
+    // }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      inject: true,
+      title: 'Crypto Ticker',
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(s(a|c)ss)$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|svg|jpe?g|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: [
+              '@babel/plugin-proposal-object-rest-spread',
+              '@babel/plugin-transform-runtime',
+            ],
+            cacheDirectory: true,
+          },
+        },
+      },
+    ],
+  },
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    assetModuleFilename: 'img/[name]-[hash:3].[ext][query]',
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      maxInitialRequests: Infinity,
+      minSize: 0,
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name(module) {
+            const packageName = module.context.match(
+              /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+            )[1];
+            return `npm.${packageName.replace('@', '')}`;
+          },
+        },
+      },
+    },
+  },
+};
