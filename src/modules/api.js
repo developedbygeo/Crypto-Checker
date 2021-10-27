da90fd097e1b0bc313dd80d3ecdfb11f9b58c2f1
georgiosan <georgiosanagnostou@hotmail.com>
2021-10-28 01:40:19 +0300

    Implemented getData, for multiple async api queries

---
+++ src/modules/api.js
@@ -0,0 +1,20 @@
+import { handleError } from './utils.js';
+
+export default async function getData(coin, pair) {
+  const urlTickers = `https://api.coingecko.com/api/v3/coins/${coin}?tickers=true&market_data=true&community_data=false&developer_data=false&sparkline=true`;
+  const urlChart = `https://api.coingecko.com/api/v3/coins/${coin}/market_chart?vs_currency=${pair}&days=365&interval=daily`;
+  try {
+    const response = await Promise.all([fetch(urlTickers), fetch(urlChart)], {
+      mode: 'cors',
+    });
+    const finalData = await Promise.all(
+      response.map((response) =>
+        !response.ok ? handleError(`${coin} not found`) : response.json()
+      )
+    );
+    return { tickers: finalData[0], chart: finalData[1] };
+  } catch (error) {
+    handleError();
+    return null;
+  }
+}
