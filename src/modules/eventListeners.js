99f2e3df34f65a35b29508e9ad97d004eee2bc9e
georgiosan <georgiosanagnostou@hotmail.com>
2021-10-29 02:23:55 +0300

    Broke the async eventListener function up for better readability - added handleData for that purpose

--- src/modules/eventListeners.js
+++ src/modules/eventListeners.js
@@ -1,19 +1,44 @@
-import { closeError } from './utils.js';
-import { submitQuery } from './utils.js';
+import { submitQuery, closeError } from './utils.js';
 import getData from './api.js';
 import ChartGenerator from './chartGenerator.js';
+import StatsGenerator from './statsGenerator.js';
 
 export default function enableEventListeners() {
   const searchBtn = document.querySelector('.searchBtn');
   const searchForm = document.querySelector('form');
   const errorBtn = document.querySelector('.close-err');
 
+  async function handleData(chartInfo, marketInfo, pair) {
+    console.log(marketInfo);
+    const {
+      name,
+      symbol: id,
+      market_cap_rank: marketCapRank,
+      image: { large: image },
+      market_data: data,
+    } = marketInfo;
+    const statsGenerator = new StatsGenerator(
+      image,
+      name,
+      id,
+      marketCapRank,
+      data,
+      pair
+    );
+    const chartGenerator = new ChartGenerator(chartInfo);
+
+    statsGenerator.populateDOM();
+    chartGenerator.createChart(10);
+  }
+
+  // TODO maybe break up the function
   searchBtn.addEventListener('click', async (e) => {
-    const { coin, pair } = await submitQuery(e);
+    const searchField = e.target.parentElement[0];
+    if (searchField.value === '') return;
+    const { coin, pair } = submitQuery(e);
     const { tickers, chart } = await getData(coin, pair);
-    const chartGenerator = new ChartGenerator(chart.prices);
-    chartGenerator.parsePrices();
-    chartGenerator.parseDates();
+    if (tickers === undefined || chart === undefined) return;
+    handleData(chart.prices, tickers, pair);
   });
   searchForm.addEventListener('submit', (e) => {
     e.preventDefault();
