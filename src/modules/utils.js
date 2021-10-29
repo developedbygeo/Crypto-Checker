728522aacae864d5491467902fa62e57dedae239
georgiosan <georgiosanagnostou@hotmail.com>
2021-10-29 23:05:50 +0300

    Added convertToCurrency for large ints

--- src/modules/utils.js
+++ src/modules/utils.js
@@ -24,4 +24,23 @@ function submitQuery(e) {
   }
   return { coin: queryText.toLowerCase(), pair: queryPair };
 }
-export { closeError, handleError, submitQuery };
+function convertToCurrency(value) {
+  let str;
+  // Evaluation
+  if (Number(value) >= 1.0e12) {
+    str = `${(Math.abs(Number(value)) / 1.0e12).toFixed(2)}T`;
+  } else if (Number(value) >= 1.0e9) {
+    str = `${(Math.abs(Number(value)) / 1.0e9).toFixed(2)}B`;
+  } else if (Number(value) >= 1.0e6) {
+    str = `${(Math.abs(Number(value)) / 1.0e9).toFixed(2)}B`;
+  } else if (Number(value) >= 1.0e3) {
+    str = `${(Math.abs(Number(value)) / 1.0e3).toFixed(2)}K`;
+  } else if (Number.isNaN(value)) {
+    str = `Unavailable`;
+  } else {
+    str = Math.abs(Number(value));
+  }
+  return str;
+}
+
+export { closeError, handleError, submitQuery, convertToCurrency };
